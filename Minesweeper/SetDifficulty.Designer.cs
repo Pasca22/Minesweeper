f65097d721f08c2e99498dca4274a7b24df0668f
Pasca1122 <alexpasca1122@gmail.com>
2022-09-23 11:17:44 +0300

    small text mistake

--- Minesweeper/SetDifficulty.Designer.cs
+++ Minesweeper/SetDifficulty.Designer.cs
@@ -45,9 +45,10 @@ namespace Minesweeper
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(268, 101);
+            this.buttonStart.Location = new System.Drawing.Point(201, 82);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(96, 48);
+            this.buttonStart.Size = new System.Drawing.Size(72, 39);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -56,9 +57,10 @@ namespace Minesweeper
             // easy
             // 
             this.easy.AutoSize = true;
-            this.easy.Location = new System.Drawing.Point(45, 47);
+            this.easy.Location = new System.Drawing.Point(34, 38);
+            this.easy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.easy.Name = "easy";
-            this.easy.Size = new System.Drawing.Size(60, 21);
+            this.easy.Size = new System.Drawing.Size(48, 17);
             this.easy.TabIndex = 1;
             this.easy.TabStop = true;
             this.easy.Text = "Easy";
@@ -67,9 +69,10 @@ namespace Minesweeper
             // medium
             // 
             this.medium.AutoSize = true;
-            this.medium.Location = new System.Drawing.Point(45, 74);
+            this.medium.Location = new System.Drawing.Point(34, 60);
+            this.medium.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.medium.Name = "medium";
-            this.medium.Size = new System.Drawing.Size(78, 21);
+            this.medium.Size = new System.Drawing.Size(62, 17);
             this.medium.TabIndex = 2;
             this.medium.TabStop = true;
             this.medium.Text = "Medium";
@@ -78,9 +81,10 @@ namespace Minesweeper
             // hard
             // 
             this.hard.AutoSize = true;
-            this.hard.Location = new System.Drawing.Point(45, 101);
+            this.hard.Location = new System.Drawing.Point(34, 82);
+            this.hard.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.hard.Name = "hard";
-            this.hard.Size = new System.Drawing.Size(60, 21);
+            this.hard.Size = new System.Drawing.Size(48, 17);
             this.hard.TabIndex = 3;
             this.hard.TabStop = true;
             this.hard.Text = "Hard";
@@ -89,65 +93,72 @@ namespace Minesweeper
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 158);
+            this.label1.Location = new System.Drawing.Point(32, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.Size = new System.Drawing.Size(127, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Select your own dificulty:";
+            this.label1.Text = "Select your own difficulty:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(86, 196);
+            this.textBox1.Location = new System.Drawing.Point(64, 159);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(32, 22);
+            this.textBox1.Size = new System.Drawing.Size(25, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(199, 196);
+            this.textBox2.Location = new System.Drawing.Point(149, 159);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(32, 22);
+            this.textBox2.Size = new System.Drawing.Size(25, 20);
             this.textBox2.TabIndex = 6;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(326, 196);
+            this.textBox3.Location = new System.Drawing.Point(244, 159);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(32, 22);
+            this.textBox3.Size = new System.Drawing.Size(25, 20);
             this.textBox3.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 199);
+            this.label2.Location = new System.Drawing.Point(24, 162);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "Width:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(140, 199);
+            this.label3.Location = new System.Drawing.Point(105, 162);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "Height:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(265, 199);
+            this.label4.Location = new System.Drawing.Point(199, 162);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.Size = new System.Drawing.Size(42, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "Bombs:";
             // 
             // SetDifficulty
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 247);
+            this.ClientSize = new System.Drawing.Size(293, 201);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -159,6 +170,7 @@ namespace Minesweeper
             this.Controls.Add(this.medium);
             this.Controls.Add(this.easy);
             this.Controls.Add(this.buttonStart);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SetDifficulty";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SetDifficulty";
